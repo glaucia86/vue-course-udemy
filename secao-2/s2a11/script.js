1cead6b1bf138b006a0818970042e798f4afccb6
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-10-11 17:49:17 -0300

    Realização do exercício s2a11

---
+++ secao-2/s2a11/script.js
@@ -0,0 +1,19 @@
+/**
+ * Arquivo: script.js
+ * Descrição: arquivo responsável por tratar a lógica na parte do script de
+ * um componente Vue.
+ * Data: 11/10/2018
+ * Author: Glaucia Lemos
+ */
+
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Olá Pessoal!'
+    },
+    methods: {
+        cumprimentar: function() {
+            return this.title;
+        }
+    }
+});
