2d7b36c308005327095a1fc7d3f29b0035069048
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-12-03 14:02:08 -0200

    Uso de prevent em Vuejs

---
+++ secao-2/s2a19/script.js
@@ -0,0 +1,25 @@
+/**
+ * Arquivo: script.js
+ * Descrição: arquivo responsável por tratar a lógica na parte do script de
+ * um componente Vue.
+ * Data: 03/12/2018
+ * Author: Glaucia Lemos
+ */
+
+new Vue({
+    el: '#app',
+    data: {
+        contador: 0,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        incrementar: function(pular, event) {
+            this.contador += pular;
+        },
+        atualizarCoordenadas: function(event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        }
+    }
+});
