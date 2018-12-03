35af840d135ca615318ddd99ba83c0f4f8334ce4
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-12-03 14:37:26 -0200

    Realização do exercício inerente a uso de events no Vuejs

---
+++ secao-2/exercicios-02/script.js
@@ -0,0 +1,19 @@
+/**
+ * Arquivo: script.js
+ * Descrição: arquivo responsável por tratar a lógica na parte do script de
+ * um componente Vue.
+ * Data: 03/12/2018
+ * Author: Glaucia Lemos
+ */
+
+new Vue({
+    el: '#exercicio',
+    data: {
+        value: ""
+    },
+    methods: {
+        clicarAqui: function() {
+            alert("Oi!");
+        }
+    }
+});
