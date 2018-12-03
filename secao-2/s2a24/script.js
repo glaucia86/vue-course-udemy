d58db93d9b8e3c4e930088ee9b40899ed90e6b41
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-12-03 17:34:35 -0200

    Exercícios sobre uso de watch em vuejs

---
+++ secao-2/s2a24/script.js
@@ -0,0 +1,35 @@
+/**
+ * Arquivo: script.js
+ * Descrição: arquivo responsável por tratar a lógica na parte do script de
+ * um componente Vue.
+ * Data: 03/12/2018
+ * Author: Glaucia Lemos
+ */
+
+new Vue({
+    el: '#app',
+    data: {
+        contador: 0,
+        segundoContador: 0
+    },
+    computed: {
+        saida: function() {
+            console.log("Computed");
+            return this.contador > 5 ? 'Maior do que 5' : 'Menor do que 5';
+        }
+    },
+    watch: {
+        contador: function(valor) {
+            var vm = this;
+            setTimeout(function() {
+                vm.contador = 0;
+            }, 2000)
+        }
+    },
+    methods: {
+        resultado: function() {
+            console.log("Method")
+            return this.console > 5 ? 'Maior do que 5' : 'Menor do que 5';
+        }
+    }
+});
