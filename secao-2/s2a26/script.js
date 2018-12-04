2737844269b5a2458fea8b52910ba3da624277be
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-12-04 15:17:18 -0200

    Exercício sobre uso de classes dinamicas

--- secao-2/s2a26/script.js
+++ secao-2/s2a26/script.js
@@ -2,14 +2,22 @@
  * Arquivo: script.js
  * Descrição: arquivo responsável por tratar a lógica na parte do script de
  * um componente Vue.
- * Data: 03/12/2018
+ * Data: 04/12/2018
  * Author: Glaucia Lemos
  */
 
 new Vue({
-    el: '#app',
+    el: "#app",
     data: {
-        contador: 0,
-        segundoContador: 0
+        colorirVermelhor: false,
+        cor: 'verde'
+    },
+    computed: {
+        divClasses: function() {
+            return {
+                vermelho: this.colorirVermelhor,
+                azul: !this.colorirVermelhor
+            };
+        }
     }
 });
---
+++ secao-2/s2a27/script.js
@@ -0,0 +1,15 @@
+/**
+ * Arquivo: script.js
+ * Descrição: arquivo responsável por tratar a lógica na parte do script de
+ * um componente Vue.
+ * Data: 04/12/2018
+ * Author: Glaucia Lemos
+ */
+
+new Vue({
+    el: '#app',
+    data: {
+        contador: 0,
+        segundoContador: 0
+    }
+});
