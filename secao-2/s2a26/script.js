a2d32e6d39e269dca8247c5a305ec255ec0874b6
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-12-04 13:21:50 -0200

    Exercícios sobre uso de two data binding.

---
+++ secao-2/s2a26/script.js
@@ -0,0 +1,15 @@
+/**
+ * Arquivo: script.js
+ * Descrição: arquivo responsável por tratar a lógica na parte do script de
+ * um componente Vue.
+ * Data: 03/12/2018
+ * Author: Glaucia Lemos
+ */
+
+new Vue({
+    el: '#app',
+    data: {
+        contador: 0,
+        segundoContador: 0
+    }
+});
